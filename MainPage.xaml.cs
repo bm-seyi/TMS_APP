887ee9b54ec605d8a8085bf1f2dff8ee1e1a6e7f
Seyi <seyiadeyemi34@outlook.com>
2024-05-01 10:34:21 +0100

    0.0.5

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.Maui.Controls;
-using System.Text.RegularExpressions;
-using System.Net.Http;
-using System.Text.Json;
-using System.Net.Http.Headers;
+using TMS_APP.Utilities;
 
 namespace TMS_APP;
 
@@ -15,11 +12,11 @@ public partial class MainPage : ContentPage
 
 	private void TextChanged_PasswordMatch(object sender, TextChangedEventArgs e)
 	{
-		((Entry)sender).BackgroundColor = (Entry_signupPassword.Text != e.NewTextValue) ? Color.FromRgb(255, 0, 0) : Colors.Transparent;
+		((Entry)sender).BackgroundColor = (entry_signupPassword.Text != e.NewTextValue) ? Color.FromRgb(255, 0, 0) : Colors.Transparent;
 	}
 
 	private void Unfocused_PasswordMatch(object sender, FocusEventArgs e){
-		if (string.IsNullOrEmpty(Entry_ConfirmedPassword.Text))
+		if (string.IsNullOrEmpty(entry_signupConPassword.Text))
 		{
 			((Entry)sender).BackgroundColor = Colors.Transparent;
 		}
@@ -27,12 +24,14 @@ public partial class MainPage : ContentPage
 
 	private void clicked_signupSubmit(object sender, EventArgs e)
 	{
-		string password = Entry_signupPassword.Text;
-		string confirmedpassword = Entry_ConfirmedPassword.Text;
-
-		if (IsValidEmailAddress(Entry_signupEmail.Text) && Entry_signupPassword.Text == Entry_ConfirmedPassword.Text)
+		if (Utils.IsValidEmail(entry_signupEmail.Text) && entry_signupPassword.Text == entry_signupConPassword.Text)
 		{
-			PostRegistration(Entry_signupEmail.Text, Entry_ConfirmedPassword.Text);
+			var data = new Dictionary<string, string>
+			{
+				{"email", entry_signupEmail.Text}, 
+				{"Pwd", entry_signupConPassword.Text},
+			};
+			API_Utilities.PostDataToAPI(data, "Registration/");
 		}
 	}
 
@@ -54,49 +53,15 @@ public partial class MainPage : ContentPage
 
 	private void clicked_loginSubmit(object sender, EventArgs e)
 	{
-		if (IsValidEmailAddress(entry_loginEmail.Text))
+		if (Utils.IsValidEmail(entry_loginEmail.Text))
 		{
-			PostRegistration(entry_loginEmail.Text, entry_loginPassword.Text);
-		}
-	}
-
-	private bool IsValidEmailAddress(string email)
-	{
-		Regex regex = new Regex(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
-		return regex.IsMatch(email);
-	}
-
-	private async void PostRegistration(string email, string password)
-	{
-		using (HttpClient client = new HttpClient()) 
-		{
-			string endpoint = "http://localhost:5188/";
-
 			var data = new Dictionary<string, string>
 			{
-				{"email", email}, 
-				{"Pwd", password},
+				{"email", entry_loginEmail.Text}, 
+				{"Pwd", entry_loginPassword.Text},
 			};
 
-			var JSON_data = JsonSerializer.Serialize(data);
-			client.BaseAddress = new Uri(endpoint);
-			client.DefaultRequestHeaders.Accept.Clear();
-			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-			client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json");
-
-			var content = new StringContent(JSON_data, System.Text.Encoding.UTF8, "application/json");
-
-			var post_registration = await client.PostAsync("Registration/", content);
-
-			if (post_registration.IsSuccessStatusCode)
-			{
-				var response = await post_registration.Content.ReadAsStringAsync();
-				Console.WriteLine(response);
-			} else 
-			{
-				Console.WriteLine(post_registration.StatusCode);
-			}
-
+			API_Utilities.PostDataToAPI(data, "Authentication/");
 		}
 	}
 }
---
+++ Utilities/Misc.cs
@@ -0,0 +1,12 @@
+using System.Text.RegularExpressions;
+
+namespace TMS_APP.Utilities;
+
+public class Utils 
+{
+    public static bool IsValidEmail(string email)
+	{
+		Regex regex = new Regex(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
+		return regex.IsMatch(email);
+	}
+}
