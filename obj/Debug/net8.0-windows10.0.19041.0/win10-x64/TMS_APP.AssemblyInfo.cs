78481b9e86c937dbc9c0b1ad3a4a1b4e0b6fa2a4
Seyi <seyiadeyemi34@outlook.com>
2024-05-01 11:03:05 +0100

    0.0.6

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/TMS_APP.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/TMS_APP.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TMS_APP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a35ee523d3e7f2b5d908e6741c460b63913d7065")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+887ee9b54ec605d8a8085bf1f2dff8ee1e1a6e7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TMS_APP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TMS_APP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
