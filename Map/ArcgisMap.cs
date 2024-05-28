37d25ca43c229ab16a5e66ea3d58971e409290ea
Seyi <seyiadeyemi34@outlook.com>
2024-05-28 14:53:00 +0100

    0.0.12

--- Map/ArcgisMap.cs
+++ Map/ArcgisMap.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using Map = Esri.ArcGISRuntime.Mapping.Map;
 using Esri.ArcGISRuntime.Ogc;
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Symbology;
 
 namespace TMS_APP.ArcgisMap
 {
@@ -13,10 +15,26 @@ namespace TMS_APP.ArcgisMap
 
         public MapViewModel()
         {
-            Map newMap = InitializeMap();
+            tmsMap = InitializeMap();
 
-            newMap.OperationalLayers.Add(kmlLayer("https://raw.githubusercontent.com/Big-Man-Seyi/metrolinkData/main/Metrolink_Lines_Functional.kml"));
-            newMap.OperationalLayers.Add(kmlLayer("https://raw.githubusercontent.com/Big-Man-Seyi/metrolinkData/main/Metrolink_Stops_Functional.kml"));
+            string[] lines  =
+            [
+                "https://github.com/Big-Man-Seyi/metrolinkData/raw/main/MetrolinkMap/Dual%20Line%20Coloured/airport_colored.kml",
+                "https://github.com/Big-Man-Seyi/metrolinkData/raw/main/MetrolinkMap/Dual%20Line%20Coloured/altrincham_colored.kml",
+                "https://github.com/Big-Man-Seyi/metrolinkData/raw/main/MetrolinkMap/Dual%20Line%20Coloured/ashton_coloured.kml",
+                "https://github.com/Big-Man-Seyi/metrolinkData/raw/main/MetrolinkMap/Dual%20Line%20Coloured/bury_coloured.kml",
+                "https://github.com/Big-Man-Seyi/metrolinkData/raw/main/MetrolinkMap/Dual%20Line%20Coloured/city_centre_coloured.kml",
+                "https://github.com/Big-Man-Seyi/metrolinkData/raw/main/MetrolinkMap/Dual%20Line%20Coloured/east_didsbury_coloured.kml",
+                "https://github.com/Big-Man-Seyi/metrolinkData/raw/main/MetrolinkMap/Dual%20Line%20Coloured/eccles_coloured.kml",
+                "https://github.com/Big-Man-Seyi/metrolinkData/raw/main/MetrolinkMap/Dual%20Line%20Coloured/oldham_rochdale_coloured.kml",
+                "https://github.com/Big-Man-Seyi/metrolinkData/raw/main/MetrolinkMap/Dual%20Line%20Coloured/trafford_centre_coloured.kml"
+            ];
+
+            foreach (string line in lines)
+            {
+                tmsMap.OperationalLayers.Add(AddLayer(line));
+            }
+
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
@@ -26,7 +44,7 @@ namespace TMS_APP.ArcgisMap
         }
 
         private Map? _map;
-        public Map? TMSMap
+        public Map? tmsMap
         {
             get => _map;
             set
@@ -39,15 +57,15 @@ namespace TMS_APP.ArcgisMap
         private Map InitializeMap()
         {
 
-            TMSMap = new Map(BasemapStyle.ArcGISDarkGray);
-            MapPoint StartingPoint = new MapPoint(-2.244644, 53.483959, SpatialReferences.Wgs84);
-            TMSMap.InitialViewpoint = new Viewpoint(StartingPoint, 100000);
+            Map map = new Map(BasemapStyle.ArcGISDarkGray)
+            {
+                InitialViewpoint = new Viewpoint(new MapPoint(-2.244644, 53.483959, SpatialReferences.Wgs84), 100000)
+            };
 
-            return TMSMap;
+            return map;
         }
 
-        private static KmlLayer kmlLayer(string url) => new KmlLayer(new KmlDataset(new Uri(url)));
-
+        private static KmlLayer AddLayer(string url) => new KmlLayer(new KmlDataset(new Uri(url)));
     }
 
 }
--- obj/Debug/net8.0-maccatalyst/maccatalyst-x64/TMS_APP.AssemblyInfo.cs
+++ obj/Debug/net8.0-maccatalyst/maccatalyst-x64/TMS_APP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TMS_APP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7b743f7e9d7c401ef7bdcce32ac8d014e897c633")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+1917466bb4ee23ad2c103285a743f7217b4d2b4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TMS_APP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TMS_APP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
