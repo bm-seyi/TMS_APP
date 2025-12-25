e16a907d70f221f4ec6e96ad45a240ad629ceddd
bm-seyi <137287174+bm-seyi@users.noreply.github.com>
2025-12-25 11:51:05 +0000

    Remove unused authentication and browser services from the project

--- TMS.App/InternalMessages/ProtocolUriMessage.cs
+++
@@ -1,9 +0,0 @@
-using CommunityToolkit.Mvvm.Messaging.Messages;
-
-namespace TMS_APP.InternalMessages
-{
-    public class ProtocolUriMessage : ValueChangedMessage<string>
-    {
-        public ProtocolUriMessage(string uri) : base(uri) { }
-    }
-}
