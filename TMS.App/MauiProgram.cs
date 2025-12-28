6578997cc5c6cea7b80f574d8461ce2a0eb7f70d
bm-seyi <137287174+bm-seyi@users.noreply.github.com>
2025-12-28 19:39:34 +0000

    Refactor MauiProgram by removing unused services and configurations; delete AuthResult and LinesModel classes

--- TMS.App/MauiProgram.cs
+++ TMS.App/MauiProgram.cs
@@ -2,13 +2,7 @@
 using System.Diagnostics.CodeAnalysis;
 using Esri.ArcGISRuntime.Maui;
 using Esri.ArcGISRuntime;
-using IBrowser = Duende.IdentityModel.OidcClient.Browser.IBrowser;
-using DotNetEnv;
 using TMS_APP.Pages;
-using TMS_APP.AccessControl;
-using TMS_APP.HubServices;
-using TMS_APP.MauiProgramExtension;
-using TMS_APP.OIDC;
 using Microsoft.Extensions.Configuration;
 
 
@@ -35,24 +29,10 @@ namespace TMS_APP
 
 			builder.Services.AddTransient<AppShell>();
 			builder.Services.AddTransient<Hub>();
-			builder.Services.AddTransient<IBrowser, BrowserService>();
-			builder.Services.AddTransient<IAuthService, AuthService>();
-			builder.Services.AddTransient<IAuthClient, AuthClient>();
 
 
 			builder.Services.AddSingleton<AccessPortal>();
-			builder.Services.AddSingleton<ILinesHubService, LinesHubService>();
-			builder.Services.AddSingleton<ISecureStorage>(SecureStorage.Default);
-
-			builder.Services.AddOidcAuthentication(options =>
-			{
-				options.Authority = "https://localhost:8443/realms/maui_realm";
-				options.ClientId = "maui_client";
-				options.Scope = "signalR.read offline_access";
-				options.RedirectUri = "tmsapp://callback/";
-				options.PostLogoutRedirectUri = "tmsapp://logout-callback/";
-				options.DisablePushedAuthorization = false;
-			});
+			builder.Services.AddSingleton(SecureStorage.Default);
 
 
 			builder.Services.AddLogging(configure =>
