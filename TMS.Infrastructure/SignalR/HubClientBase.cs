3e76f95a79ddc104d7bc15e64786a9282f61e093
S <137287174+bm-seyi@users.noreply.github.com>
2026-05-03 19:20:03 +0100

    Add SignalR client implementation and configuration extensions
    - Introduced `HubClientBase` for managing SignalR connections.
    - Created `LinesDataHubClient` to handle line updates via SignalR.
    - Added `ConfigurationExtension` for retrieving required configuration values.
    - Updated `appsettings.Development.Json` for SignalR configuration.
    - Included `Microsoft.AspNetCore.SignalR.Client` package in project dependencies.

---
+++ TMS.Infrastructure/SignalR/HubClientBase.cs
@@ -0,0 +1,57 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Logging;
+using Xamarin.Google.Crypto.Tink.Proto;
+
+namespace TMS.Infrastructure.SignalR;
+
+internal abstract class HubClientBase(ILogger logger, ActivitySource activitySource) : IAsyncDisposable
+{
+    private readonly ILogger _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    private readonly ActivitySource _activitySource = activitySource ?? throw new ArgumentNullException(nameof(activitySource));
+
+    protected HubConnection? Connection;
+
+    protected abstract string HubUrl { get; }
+
+    public bool IsConnected => Connection?.State == HubConnectionState.Connected;
+
+    public async Task ConnectAsync()
+    {
+        using Activity? _ = _activitySource.StartActivity(".ConnectAsync");
+
+        if (Connection != null && IsConnected)
+            return;
+
+        Connection = new HubConnectionBuilder()
+            .WithUrl(HubUrl)
+            .WithAutomaticReconnect()
+            .Build();
+
+        RegisterHandlers(Connection);
+
+        await Connection.StartAsync();
+    }
+
+    public async Task DisconnectAsync()
+    {
+        if (Connection == null)
+            return;
+
+        if (Connection.State != HubConnectionState.Disconnected)
+            await Connection.StopAsync();
+    }
+
+    protected abstract void RegisterHandlers(HubConnection connection);
+
+    public async ValueTask DisposeAsync()
+    {
+        if (Connection != null)
+        {
+            await DisconnectAsync();
+            await Connection.DisposeAsync();
+            Connection = null;
+        }
+        GC.SuppressFinalize(this);
+    }
+}
